e4e43968e951d4055f05f41ec112c3242d08cd84
zhiminV <122182731+zhiminV@users.noreply.github.com>
2024-03-30 11:01:06 -0700

    set up the environment and some code structure

---
+++ Project5_Recognition_using_Deep_Networks/ DeepNetwork.py
@@ -0,0 +1,32 @@
+# Zhimin Liang
+# Spring 2024
+# 5330 Project 5
+
+# Purpose:
+
+# import statements
+import sys
+
+# class definitions
+class MyNetwork(nn.Module):
+    def __init__(self):
+        pass
+
+# computes a forward pass for the network
+# methods need a summary comment
+def forward(self, x):
+    return x
+
+# useful functions with a comment for each function
+def train_network( arguments ):
+    return
+
+# main function (yes, it needs a comment too)
+def main(argv):
+# handle any command line arguments in argv
+
+# main function code
+    return
+
+if __name__ == "__main__":
+    main(sys.argv)
